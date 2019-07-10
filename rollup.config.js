9e98c4ae54857bcd5d66cf15908ff58d46442c10
Nikita Popov <nckcol@gmail.com>
2019-07-10 23:10:20 +0300

    chore: prepare package to publish

--- rollup.config.js
+++ rollup.config.js
@@ -4,7 +4,7 @@ import {sizeSnapshot} from 'rollup-plugin-size-snapshot';
 export default {
   input: './src/index.js',
   output: {
-    file: './esm/index.js',
+    file: './esm/index.mjs',
     format: 'esm',
   },
   external: ['react', 'react-dom'],
